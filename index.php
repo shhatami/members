bb3507ae5900f7a1ed0c27db6a0f02cb942648bb
hatami <53007257+shhatami@users.noreply.github.com>
2022-09-25 17:13:00 +0330

    Add files via upload

---
+++ index.php
@@ -0,0 +1,336 @@
+<!DOCTYPE HTML>  
+<html lang="fa" lang="en" dir="rtl"> <head>
+    <meta charset="UTF-8">
+
+<title>اوگوفای: سایت خرید تور</title>
+
+    <link rel="icon" type="image/x-icon" href="http://i66.tinypic.com/2m5ff5j.png">
+    
+    
+    <meta name="keywords" content="ogofy,اوگوفای,offer,تخفیف"/>
+    <meta name="discription" content="خرید تور,خرید تورآنلاین,تخفیف توخفیف,ر,اینترنتی,travel,سفر"/>
+    
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tahoma">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+        .error {color: #FF0000;}
+    html{
+        overflow-y: scroll;
+        }
+         body,h1,h2,h3,h4,h5,h6{font-family:Lato, sans-serif; font-size:20px;}
+       .w3-bar,h1,button {font-family:Montserrat, sans-serif;}
+       .fa-anchor,.fa-coffee {font-size:200px}
+    body{
+            margin:0;
+        }
+        
+    header
+        {
+            font-size: 10px;
+        }
+    .subheader 
+        {
+        text-align:center;
+        overflow:auto;
+        padding:60px;
+        top:47px;
+        right:0px;
+        left:0px;
+        background-color:rgb(5,30,150);
+        width: 1600px;
+        height: 150px;
+        }
+          #container
+        {
+  /* all browsers except ie6 will respect the !important flag */
+        height: auto !important;
+        min-height: 500px;
+  /* Should have the same value as the min height above */
+        height: 500px;
+        }
+        a[target="_blank"]:before,
+        a[target="new"]:before {
+        margin: 0 5px 0 0;
+        padding: 1px;
+        outline: 1px solid #333;
+        color: #333;
+        background: #ff9;
+        font: 12px "Zapf Dingbats";
+        content: "\279C";
+        }
+        
+    input[type=text]{
+        width: 18%;
+        box-sizing: border-box;
+        border: 2px solid #ccc;
+        border-radius: 10px;
+        font-size: 16px;
+        background-color: white;
+        background-image: url('searchicon.png');
+        background-position: 10px 10px; 
+        background-repeat: no-repeat;
+        padding: 8px 30px 10px 20px;
+        }
+        
+    .content
+        {
+        position:absolute;
+        text-align:center;
+        overflow:auto;
+        min-height:300px;
+        max-height:1280px;
+        padding:10px 80px 0px 80px;
+        top:150px;
+        right:0px;
+        left:0px;
+        background-color: hsla(9, 80%, 64%, 0.5);
+        width: 1525px;
+        height: 1400px;
+        }
+    .subcontent
+        {
+        position:absolute;
+        text-align:left;
+        overflow:auto;
+        min-height:200px;
+        max-height:400px;
+        padding:10px 80px 0px 80px;
+        top:1430px;
+        right:0px;
+        left:0px;
+        background-color:#f0f0f0;
+        width: 1550px;
+        height:300px;
+        }
+
+    .footer
+        {
+        position:absolute;
+        background-color:#787878;
+        text-align:rtl;
+        right:0px;
+        left:0px;
+        height:250px;
+        margin-top:1560px;
+        width:1600px;
+        font-family:sans-serif;
+        font-size:16px;
+        padding:10px 80px 0px 80px;
+        }
+        </style>
+
+</head>
+<body> 
+    <header>
+    <!-- Navbar or header -->
+        <div class="w3-top">
+        <div class="w3-bar w3-white w3-card w3-left-align w3-large">
+    <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-gray w3-large w3-yellow" href="javascript:void(0);" onclick="myFunction()" 
+        title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+    <a href="#"><img src="http://i68.tinypic.com/rii55e.jpg" border="0" alt="Image and video hosting by TinyPic" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white"></a>
+    <a href="http://localhost/faradars/original/index.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">خانه</a>
+    <a href="http://localhost:8383/%D9%88%D8%B1%D9%88%D8%AF/index.html" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">ورود</a>
+    <a href="http://localhost/faradars/remember1/index.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">ثبت نام</a>
+    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">ایرانگردی</a>
+        </div>
+
+        </div>
+</header>
+    
+<subheader>
+          <!-- subheader -->
+        <div class="subheader">
+             
+    <a href="#"><img src="/w3images/avatar_g2.jpg" style="width:100px;" class="w3-circle w3-right w3-margin w3-hide-large w3-hover-opacity"></a>
+        <span class="w3-button w3-hide-large w3-xxlarge w3-hover-text-grey" onclick="w3_open()"><i class="fa fa-bars"></i></span>
+    
+        <div class="w3-container">
+        <h1><b></b></h1></div>
+   
+        <div>
+        <label>
+        <i class="fa fa-calendar-o"></i>
+        <input class="form-control w3-large oneline-checkin checkin-input date-input" type="text" data-selenium="checkInBox" placeholder="DD MM YYYY" name="تاریخ رفت" required>
+        <i class="fa fa-calendar-o"></i>
+        <input class="form-control w3-large oneline-checkin checkin-input date-input" type="text" data-selenium="checkInBox" placeholder="DD MM YYYY" name="تاریخ برگشت" required>
+        <i class="fa"></i>
+        <input class="form-control w3-large oneline-checkin checkin-input date-input" type="text" data-selenium="checkInBox" placeholder="مقصد" name="مقصد" required>
+        <input class="form-control w3-large oneline-checkin checkin-input date-input" type="text" name="search" placeholder="جستجو ...">
+          
+        </label>
+       
+           
+        </div>
+        </div>
+</subheader>
+
+        <div class="content">
+
+<?php
+// define variables and set to empty values
+$nameErr =  $familynameErr = $emailErr = $genderErr = $passwordErr = $cityErr = "";
+$name = $email = $familyname = $city = $gender = $password = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed";
+    }
+  }
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format";
+    }
+  }
+  
+    if (empty($_POST["familyname"])) {
+    $familynameErr = "familyname is required";
+  } else {
+    $familyname = test_input($_POST["familyname"]);
+    // check if familyname only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+      $familynameErr = "Only letters and white space allowed";
+    }
+  }
+  
+   if (empty($_POST["city"])) {
+    $cityErr = "city is required";
+  } else {
+    $city = test_input($_POST["city"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$city)) {
+      $cityErr = "Only letters and white space allowed";
+    }
+  }
+  
+  
+  if (empty($_POST["password"])) {
+    $passwordErr = "password is required";
+  } else {
+    $password = test_input($_POST["password"]);
+    // check if password only contains letters and whitespace
+    if (!PASSWORD_DEFAULT("/^[a-zA-Z]*$/",$password)) {
+      $passwordErr = "Only letters and white space and number allowed";
+    }
+  }
+  
+
+  if (empty($_POST["gender"])) {
+    $genderErr = "Gender is required";
+  } else {
+    $gender = test_input($_POST["gender"]);
+  }
+  
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+<h2>فرم ثبت نام در سایت اوگوفای</h2>
+<p><span class="error">* مواردی که الزاما باید وارد شود</span></p>
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
+    
+  <input type="text" name="name" placeholder="نام" value="<?php echo $name;?>">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br><br>
+  
+  <input type="text" name="familyname" placeholder="نام خانوادگی" value="<?php echo $familyname;?>">
+  <span class="error">* <?php echo $familynameErr;?></span>
+  <br><br>
+  
+  
+  <input type="text" name="email" placeholder="ایمیل" value="<?php echo $email;?>">
+  <span class="error">* <?php echo $emailErr;?></span>
+  <br><br>
+  
+  <input type="text" name="password" placeholder="پسورد" value="<?php echo $password;?>">
+  <span class="error">* <?php echo $passwordErr;?></span>
+  <br><br>
+  
+  <input type="text" name="city" placeholder="شهر محل سکونت" value="<?php echo $city;?>">
+  <span class="error">* <?php echo $cityErr;?></span>
+  <br><br>
+   
+  جنسیت:
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">زن
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">مرد
+
+  <span class="error">* <?php echo $genderErr;?></span>
+  <br><br>
+  <input type="submit" name="submit" value="عضویت">  
+</form>
+
+<?php
+echo "<h2></h2>";
+echo $name;
+echo "<br>";
+echo $familyname;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $city;
+echo "<br>";
+echo $gender;
+?>
+        </div>
+
+<div class ="subcontent" class="w3-bar w3-red w3-card w3-left-align w3-large">
+    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-gray" style="text-align:left; font-size:15px;">بلاگ</a>
+    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-gray" style="text-align:left; font-size:15px;">راهنما</a>
+    <p style="text-align:right; font-size:12px; text-indent: top; ">
+        همه ما به سفر نیاز داریم، سفری هر چند کوتاه.<br>
+        هر کسی به طریقی به دنبال یافتن یک مسیر خوب با قیمت مناسب در تاریخ مورد نظر<br>
+       خود میباشد. شرکت اوگوفای تصمیم دارد شما را در مسیر یافتن بهترینها همراهی نماید.<br>
+       این سایت با دیدگاه، ارائه تور با اهداف : قیمت مناسب، راحتی در سفر، اعتماد در خرید،<br>
+        ارائه بهترین ها، ایجاد شده است. تمامی خدمات این سایت،از طریق آژانس های باسابقه<br>
+        و مطمئن و دارای مجوز صورت گرفته است. در صورت هر گونه تخلف از طرف هریک از <br>
+        آنها مطابق قوانین در بخش فروش، خسارت پیش آمده، جبران خواهد شد.<br>
+        همراهی شما در سفر، افتخار ماست.<br>
+        </p>
+
+       
+
+        </div>
+
+
+  <div class="w3-container" id="contact">
+  
+      <footer class ="footer" style="text-align:center;" ><br><br><br><br><br>
+                  <i class="fa fa-instagram w3-margin w3-hover-opacity"></i>
+        <i class="fa fa-pinterest-p w3-margin w3-hover-opacity"></i>
+        <i class="fa fa-twitter w3-margin w3-hover-opacity"></i>
+        <i class="fa fa-linkedin w3-margin w3-hover-opacity"></i>
+        <br>
+          <i class="fa fa-envelope w3-text-red w3-margin" style="width:10px;"> </i> Email: help@ogofy.com
+          <i class="fa fa-phone w3-text-red w3-margin" style="width:10px;"></i> Phone: 02634300001
+          <i class="fa fa-map-marker w3-text-red w3-margin" style="width:10px;"></i> ایران ، تهران، کرج
+       
+      
+
+  
+</footer>
+          </div>  
+        <?php
+        // put your code here
+        ?>
+</body>
+</html>
